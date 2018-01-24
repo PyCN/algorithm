5ad58c959efcf1d291bb7b4f566cc22d9f0e800d
hduyyg <2632158294@qq.com>
2018-01-24 22:32:57 +0800

    modify for github addr in leetcode/33

--- leetcode/33/hduyyg.py
+++ leetcode/33/hduyyg.py
@@ -1,5 +1,5 @@
 # author:2632158294@qq.com
-# github:https://github.com/PyCN/algorithm/tree/master/leetcode/32
+# github:https://github.com/PyCN/algorithm/tree/master/leetcode/33
 # time: O(n)
 
 class Solution:
